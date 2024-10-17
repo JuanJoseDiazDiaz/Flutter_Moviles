70079d2ac6a77657735a6e8ba7ce0eb6a0860512
elpapass <115544415+elpapass@users.noreply.github.com>
2024-10-17 10:59:10 +0200

    -- in process

--- lib/ejemplo_layout.dart
+++ lib/ejemplo_layout.dart
@@ -7,19 +7,67 @@ class EjemploLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My App"),
-        backgroundColor: Colors.red,
-        centerTitle: true,
-        actions: [
-          Text("1"),
-          Text("2"),
+      // appBar: AppBar(
+      //   title: Text("My App"),
+      //   backgroundColor: Colors.red,
+      //   centerTitle: true,
+      //   actions: [
+      //     Text("1"),
+      //     Text("2"),
+      //   ],
+      //   leading: Text("Volver"),
+      // ),
+      // backgroundColor: Colors.blue,
+      backgroundColor: Colors.red,
+      body: Column(
+         // mainAxisSize: MainAxisSize.max,
+        // verticalDirection: VerticalDirection.up,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.grey,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.black,
+              ),
+            ],
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.white,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
         ],
-        leading: Text("Volver"),
+        // child: Image.asset('assets/imagenes/FotoEjercicio-200x300.jpg'),
       ),
-      backgroundColor: Colors.blue,
-      body: Center(
-          child: Image.asset('assets/imagenes/FotoEjercicio-200x300.jpg')),
     );
   }
 }
